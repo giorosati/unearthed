f9db6d4c63d4a4f7b63725b7c5a152e136d85b31
giorosati <giovanni.rosati@gmail.com>
2026-02-25 22:16:38 -0600

    Add gift data and routes for gifts API
    - Created a new data file `gifts.js` containing an array of gift objects with details such as name, price point, audience, image, description, and submission info.
    - Implemented a new Express router in `gifts.js` to handle GET requests for all gifts and individual gift details.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 import express from 'express'
+
+import giftsRouter from './routes/gifts.js'
+
 const app = express()
 app.use('/public', express.static('./public'))
 app.use('/scripts', express.static('./public/scripts'))
@@ -6,6 +9,7 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
 })
 
+app.use('/gifts', giftsRouter)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
